2ee112affca7a67e3e3aeded8968979ef0182f8e
Yeliz Taneroğlu <yeliztaneroglu@gmail.com>
2016-11-09 01:03:13 +0300

    Veritabanındaki postları görebilmek için template'i değiştirdim.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
-
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
