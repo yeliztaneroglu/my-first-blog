43fd3c3e7fac242a191de9f0bcd07b9856b66189
Yeliz Taneroğlu <yeliztaneroglu@gmail.com>
2016-11-09 16:40:28 +0300

    Web sitesine güncelleme ve yaratma için view eklendi.

--- blog/views.py
+++ blog/views.py
@@ -26,3 +26,22 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.yazar = request.user
+            post.yayinlama_tarihi = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
